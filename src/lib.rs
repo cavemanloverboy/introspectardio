4b45068e3964cc0ae581b57e08638560b61ee9d5
cavemanloverboy <caveycool@gmail.com>
2025-11-27 07:40:31 -0500

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,276 @@
+#![allow(unexpected_cfgs)]
+
+use core::array::from_ref as b;
+
+use const_crypto::bs58;
+use pinocchio::{
+    account_info::{AccountInfo, Ref, RefMut},
+    instruction::Signer,
+    program_error::ProgramError,
+    pubkey::{find_program_address, pubkey_eq, Pubkey},
+    seeds,
+    sysvars::instructions::{Instructions, IntrospectedInstruction},
+    ProgramResult,
+};
+use pinocchio_system::create_account_with_minimum_balance_signed;
+use pinocchio_token::{instructions::Transfer, state::TokenAccount};
+
+pinocchio::entrypoint!(process);
+
+pub const ID: Pubkey = [5; 32];
+pub const TOKEN_PROGRAM: Pubkey =
+    bs58::decode_pubkey("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA");
+
+#[repr(C)]
+pub struct Pool {
+    pub usdc_atoms_per_sol: U128,
+    pub vault_a: Pubkey,
+    pub vault_b: Pubkey,
+    // seeds: [pool_key(32), mint_key(32), bump(1)] = 65 bytes
+    pub vault_a_seeds: [u8; 65],
+    pub vault_b_seeds: [u8; 65],
+}
+
+use uint::construct_uint;
+
+construct_uint! {
+    pub struct U128(2);
+}
+
+impl Pool {
+    pub const LEN: usize = core::mem::size_of::<Self>();
+
+    pub fn from_account<'a>(account: &'a AccountInfo) -> Result<Ref<'a, Self>, ProgramError> {
+        let data = account.try_borrow_data()?;
+        if data.len() < Self::LEN {
+            return Err(ProgramError::InvalidAccountData);
+        }
+        Ok(Ref::map(data, |d| unsafe { &*d.as_ptr().cast() }))
+    }
+
+    pub fn from_account_mut<'a>(
+        account: &'a AccountInfo,
+    ) -> Result<RefMut<'a, Self>, ProgramError> {
+        let data = account.try_borrow_mut_data()?;
+        if data.len() < Self::LEN {
+            return Err(ProgramError::InvalidAccountData);
+        }
+        Ok(RefMut::map(data, |d| unsafe {
+            &mut *d.as_mut_ptr().cast()
+        }))
+    }
+}
+
+pub fn process(_id: &Pubkey, accounts: &[AccountInfo], data: &[u8]) -> ProgramResult {
+    let (&disc, rest) = data
+        .split_first()
+        .ok_or(ProgramError::InvalidInstructionData)?;
+
+    match disc {
+        0 => process_init(accounts, rest),
+        1 => process_swap(accounts),
+        _ => Err(ProgramError::InvalidInstructionData),
+    }
+}
+
+fn process_init(accounts: &[AccountInfo], data: &[u8]) -> ProgramResult {
+    let [payer, pool, vault_a, vault_b, mint_a, mint_b, _system_program, _token_program] = accounts
+    else {
+        return Err(ProgramError::NotEnoughAccountKeys);
+    };
+
+    if data.len() < 8 {
+        return Err(ProgramError::InvalidInstructionData);
+    }
+
+    let usdc_atoms_per_sol = unsafe { data.as_ptr().cast::<u64>().read_unaligned() };
+
+    // derive vault PDAs
+    let (expected_vault_a, bump_a) = find_program_address(&[pool.key(), mint_a.key()], &ID);
+    let (expected_vault_b, bump_b) = find_program_address(&[pool.key(), mint_b.key()], &ID);
+
+    if !pubkey_eq(vault_a.key(), &expected_vault_a) {
+        return Err(ProgramError::InvalidSeeds);
+    }
+    if !pubkey_eq(vault_b.key(), &expected_vault_b) {
+        return Err(ProgramError::InvalidSeeds);
+    }
+
+    // init pool account
+    let mut pool_data = Pool::from_account_mut(pool)?;
+    pool_data.usdc_atoms_per_sol = U128::from(usdc_atoms_per_sol);
+    pool_data.vault_a = *vault_a.key();
+    pool_data.vault_b = *vault_b.key();
+
+    // build vault_a seeds: [pool_key, mint_a_key, bump]
+    pool_data.vault_a_seeds[0..32].copy_from_slice(pool.key());
+    pool_data.vault_a_seeds[32..64].copy_from_slice(mint_a.key());
+    pool_data.vault_a_seeds[64] = bump_a;
+
+    // build vault_b seeds: [pool_key, mint_b_key, bump]
+    pool_data.vault_b_seeds[0..32].copy_from_slice(pool.key());
+    pool_data.vault_b_seeds[32..64].copy_from_slice(mint_b.key());
+    pool_data.vault_b_seeds[64] = bump_b;
+
+    let seeds_a = seeds!(pool.key(), mint_a.key(), b(&bump_a));
+    let signer_a = Signer::from(&seeds_a);
+
+    let seeds_b = seeds!(pool.key(), mint_b.key(), b(&bump_b));
+    let signer_b = Signer::from(&seeds_b);
+
+    // create vault_a
+    create_account_with_minimum_balance_signed(
+        vault_a,
+        TokenAccount::LEN,
+        &TOKEN_PROGRAM,
+        payer,
+        None,
+        &[signer_a],
+    )?;
+
+    // init vault_a (vault owns itself)
+    pinocchio_token::instructions::InitializeAccount3 {
+        account: vault_a,
+        mint: mint_a,
+        owner: vault_a.key(),
+    }
+    .invoke()?;
+
+    // create vault_b
+    create_account_with_minimum_balance_signed(
+        vault_b,
+        TokenAccount::LEN,
+        &TOKEN_PROGRAM,
+        payer,
+        None,
+        &[signer_b],
+    )?;
+
+    // init vault_b (vault owns itself)
+    pinocchio_token::instructions::InitializeAccount3 {
+        account: vault_b,
+        mint: mint_b,
+        owner: vault_b.key(),
+    }
+    .invoke()?;
+
+    Ok(())
+}
+
+fn process_swap(accounts: &[AccountInfo]) -> ProgramResult {
+    let [_payer, pool, user_out, pool_vault_a, pool_vault_b, ix_sysvar, _token_program] = accounts
+    else {
+        return Err(ProgramError::NotEnoughAccountKeys);
+    };
+
+    let pool_data = Pool::from_account(pool)?;
+
+    // verify vaults match stored keys
+    if !pubkey_eq(pool_vault_a.key(), &pool_data.vault_a) {
+        return Err(ProgramError::InvalidSeeds);
+    }
+    if !pubkey_eq(pool_vault_b.key(), &pool_data.vault_b) {
+        return Err(ProgramError::InvalidSeeds);
+    }
+
+    let instruction_sysvar = unsafe { Instructions::new_unchecked(ix_sysvar.try_borrow_data()?) };
+    let cur_idx = instruction_sysvar.load_current_index() as usize;
+    if cur_idx == 0 {
+        return Err(IntrospectardioError::PrevIxNotTokenProgram.into());
+    }
+    let prev_idx = cur_idx - 1;
+
+    let curr_ixn =
+        unsafe { instruction_sysvar.deserialize_instruction_unchecked(cur_idx as usize) };
+    // not a cpi (or whitelisted caller)
+    if *curr_ixn.get_program_id() != ID {
+        return Err(ProgramError::IncorrectProgramId);
+    }
+
+    let prev_ix = instruction_sysvar.load_instruction_at(prev_idx)?;
+    let amount_in = validate_prev_ix(prev_ix, pool_vault_a.clone())?;
+
+    // Calculate amount out
+    let Some(Ok(amount_out)) = U128::from(amount_in)
+        .checked_mul(pool_data.usdc_atoms_per_sol)
+        .map(|x| x / 1_000_000_000)
+        .map(|x| x.try_into())
+    else {
+        return Err(IntrospectardioError::LargeOrder)?;
+    };
+
+    // Transfer out (vault_b signs for itself)
+    let pool_key = &pool_data.vault_b_seeds[0..32];
+    let mint_key = &pool_data.vault_b_seeds[32..64];
+    let bump = &pool_data.vault_b_seeds[64..65];
+    let seeds = seeds!(pool_key, mint_key, bump);
+    let signer = Signer::from(&seeds);
+
+    Transfer {
+        from: pool_vault_b,
+        to: user_out,
+        authority: pool_vault_b,
+        amount: amount_out,
+    }
+    .invoke_signed(&[signer])?;
+
+    Ok(())
+}
+
+#[repr(u32)]
+pub enum IntrospectardioError {
+    PrevIxNotTokenProgram,
+    UnexpectedTokenProgramDataLen,
+    UnexpectedTokenProgramIx,
+    UnexpectedTransferDest,
+    LargeOrder,
+}
+
+impl From<IntrospectardioError> for ProgramError {
+    fn from(value: IntrospectardioError) -> ProgramError {
+        ProgramError::Custom(value as u32)
+    }
+}
+
+// Previous instruction must be
+// 1) token program invocation
+// 2) transfer ix data len
+// 3) transfer ix disc
+// 4) transfer dest is pool vault
+//
+// If we are executing this code, it's because the instruction succeeded!
+fn validate_prev_ix(
+    prev_ix: IntrospectedInstruction,
+    pool_vault_in: AccountInfo,
+) -> Result<u64, ProgramError> {
+    // 1) token program invocation
+    if !pubkey_eq(prev_ix.get_program_id(), &TOKEN_PROGRAM) {
+        return Err(IntrospectardioError::PrevIxNotTokenProgram.into());
+    }
+    let prev_ix_data = prev_ix.get_instruction_data();
+
+    // 2) transfer ix data len
+    let correct_data_len = prev_ix_data.len() >= 9;
+    if !correct_data_len {
+        return Err(IntrospectardioError::UnexpectedTokenProgramDataLen.into());
+    }
+
+    // 3) transfer ix disc
+    const TRANSFER_DISC: u8 = 3;
+    let correct_disc = prev_ix_data[0] == TRANSFER_DISC;
+    if !correct_disc {
+        return Err(IntrospectardioError::UnexpectedTokenProgramIx.into());
+    }
+
+    // 4) transfer dest is pool vault
+    // SAFETY: transfer succeeded so num accounts is correct
+    let dest = unsafe { prev_ix.get_account_meta_at_unchecked(1) };
+    let correct_dest = dest.key.eq(pool_vault_in.key());
+    if !correct_dest {
+        return Err(IntrospectardioError::UnexpectedTransferDest.into());
+    }
+
+    // read amount in
+    let amount_in = unsafe { prev_ix_data.as_ptr().add(1).cast::<u64>().read_unaligned() };
+    Ok(amount_in)
+}
---
+++ tests/integration.rs
@@ -0,0 +1,235 @@
+use litesvm::LiteSVM;
+use solana_sdk::{
+    instruction::{AccountMeta, Instruction},
+    message::Message,
+    native_token::LAMPORTS_PER_SOL,
+    pubkey::Pubkey,
+    transaction::Transaction,
+};
+use solana_sdk_ids::system_program;
+use solana_system_interface::instruction::create_account;
+
+const PROGRAM_ID: Pubkey = Pubkey::new_from_array([5; 32]);
+const TOKEN_PROGRAM: Pubkey = solana_sdk::pubkey!("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA");
+
+const POOL_LEN: usize = 216;
+
+#[test]
+fn integration() {
+    let mut svm = LiteSVM::new()
+        .with_sigverify(false)
+        .with_blockhash_check(false)
+        .with_transaction_history(0);
+    svm.add_program_from_file(PROGRAM_ID, "target/deploy/introspectardio.so")
+        .unwrap();
+    svm.add_program_from_file(TOKEN_PROGRAM, "ptoken.so")
+        .unwrap();
+
+    let payer = Pubkey::new_unique();
+    let pool = Pubkey::new_unique();
+    let user = Pubkey::new_unique();
+
+    let mint_a = Pubkey::new_unique(); // SOL-wrapped (decimals=9)
+    let mint_b = Pubkey::new_unique(); // USDC (decimals=6)
+
+    svm.airdrop(&payer, 100 * LAMPORTS_PER_SOL).unwrap();
+    svm.airdrop(&user, 100 * LAMPORTS_PER_SOL).unwrap();
+
+    // derive vault PDAs (seeds: pool_key, mint_key)
+    let (vault_a, _) = Pubkey::find_program_address(&[pool.as_ref(), mint_a.as_ref()], &PROGRAM_ID);
+    let (vault_b, _) = Pubkey::find_program_address(&[pool.as_ref(), mint_b.as_ref()], &PROGRAM_ID);
+
+    // create mints
+    create_mint(&mut svm, &payer, &mint_a, 9); // SOL decimals
+    create_mint(&mut svm, &payer, &mint_b, 6); // USDC decimals
+
+    // create user token accounts
+    let user_ata_a = Pubkey::new_unique();
+    let user_ata_b = Pubkey::new_unique();
+    create_token_account(&mut svm, &payer, &user_ata_a, &mint_a, &user);
+    create_token_account(&mut svm, &payer, &user_ata_b, &mint_b, &user);
+
+    // mint some tokens to user
+    mint_to(
+        &mut svm,
+        &payer,
+        &mint_a,
+        &user_ata_a,
+        10 * LAMPORTS_PER_SOL,
+    );
+
+    // 1) Initialize pool
+    let pool_rent = svm.minimum_balance_for_rent_exemption(POOL_LEN);
+    let create_pool_ixn = create_account(&payer, &pool, pool_rent, POOL_LEN as u64, &PROGRAM_ID);
+
+    let usdc_atoms_per_sol: u64 = 1_000 * 1_000_000; // $1000 per SOL in USDC atoms (6 decimals)
+    let mut init_data = vec![0u8]; // discriminator
+    init_data.extend_from_slice(&usdc_atoms_per_sol.to_le_bytes());
+
+    let init_ixn = Instruction {
+        program_id: PROGRAM_ID,
+        accounts: vec![
+            AccountMeta::new(payer, true),
+            AccountMeta::new(pool, false),
+            AccountMeta::new(vault_a, false),
+            AccountMeta::new(vault_b, false),
+            AccountMeta::new_readonly(mint_a, false),
+            AccountMeta::new_readonly(mint_b, false),
+            AccountMeta::new_readonly(system_program::ID, false),
+            AccountMeta::new_readonly(TOKEN_PROGRAM, false),
+        ],
+        data: init_data,
+    };
+
+    let msg = Message::new(&[create_pool_ixn, init_ixn], Some(&payer));
+    let txn = Transaction::new_unsigned(msg);
+    let res = svm.send_transaction(txn).unwrap();
+
+    println!("Initialize Pool");
+    for log in res.logs {
+        println!("    {log}");
+    }
+
+    // mint USDC to vault_b so pool has liquidity
+    mint_to(&mut svm, &payer, &mint_b, &vault_b, 1_000_000 * 1_000_000); // 1M USDC (6 decimals)
+
+    // 2) Swap: user transfers SOL-token to vault_a, then calls swap
+    let amount_in: u64 = 1 * LAMPORTS_PER_SOL; // 1 SOL (9 decimals)
+
+    // transfer instruction (user -> vault_a)
+    let transfer_ixn = spl_token_transfer_instruction(&user_ata_a, &vault_a, &user, amount_in);
+
+    // swap instruction
+    let swap_ixn = Instruction {
+        program_id: PROGRAM_ID,
+        accounts: vec![
+            AccountMeta::new(user, true),
+            AccountMeta::new_readonly(pool, false),
+            AccountMeta::new(user_ata_b, false), // user_out
+            AccountMeta::new(vault_a, false),    // pool_vault_a
+            AccountMeta::new(vault_b, false),    // pool_vault_b
+            AccountMeta::new_readonly(solana_sdk::sysvar::instructions::ID, false),
+            AccountMeta::new_readonly(TOKEN_PROGRAM, false),
+        ],
+        data: vec![1], // swap discriminator
+    };
+
+    let before_user_b = get_token_balance(&svm, &user_ata_b);
+
+    let msg = Message::new(&[transfer_ixn, swap_ixn], Some(&user));
+    let txn = Transaction::new_unsigned(msg);
+    let res = svm.send_transaction(txn).unwrap();
+
+    println!("Swap");
+    for log in res.logs {
+        println!("    {log}");
+    }
+
+    let after_user_b = get_token_balance(&svm, &user_ata_b);
+
+    // expected: 1 SOL * 1000 USDC/SOL = 1000 USDC = 1_000_000_000 USDC atoms (6 decimals)
+    let expected_out = amount_in * usdc_atoms_per_sol / LAMPORTS_PER_SOL;
+    assert_eq!(after_user_b - before_user_b, expected_out);
+
+    println!(
+        "Swap successful: {} SOL -> {} USDC atoms",
+        amount_in, expected_out
+    );
+}
+
+fn create_mint(svm: &mut LiteSVM, payer: &Pubkey, mint: &Pubkey, decimals: u8) {
+    let mint_space = 82; // Mint::LEN
+    let rent = svm.minimum_balance_for_rent_exemption(mint_space);
+    let create_ixn = create_account(payer, mint, rent, mint_space as u64, &TOKEN_PROGRAM);
+
+    // InitializeMint2: disc=20, decimals, mint_authority, freeze_authority_option=0
+    let mut init_data = vec![20u8, decimals];
+    init_data.extend_from_slice(payer.as_ref()); // mint authority
+    init_data.push(0); // no freeze authority
+
+    let init_ixn = Instruction {
+        program_id: TOKEN_PROGRAM,
+        accounts: vec![AccountMeta::new(*mint, false)],
+        data: init_data,
+    };
+
+    let msg = Message::new(&[create_ixn, init_ixn], Some(payer));
+    let txn = Transaction::new_unsigned(msg);
+    svm.send_transaction(txn).unwrap();
+}
+
+fn create_token_account(
+    svm: &mut LiteSVM,
+    payer: &Pubkey,
+    account: &Pubkey,
+    mint: &Pubkey,
+    owner: &Pubkey,
+) {
+    let account_space = 165; // TokenAccount::LEN
+    let rent = svm.minimum_balance_for_rent_exemption(account_space);
+    let create_ixn = create_account(payer, account, rent, account_space as u64, &TOKEN_PROGRAM);
+
+    // InitializeAccount3: disc=18, owner
+    let mut init_data = vec![18u8];
+    init_data.extend_from_slice(owner.as_ref());
+
+    let init_ixn = Instruction {
+        program_id: TOKEN_PROGRAM,
+        accounts: vec![
+            AccountMeta::new(*account, false),
+            AccountMeta::new_readonly(*mint, false),
+        ],
+        data: init_data,
+    };
+
+    let msg = Message::new(&[create_ixn, init_ixn], Some(payer));
+    let txn = Transaction::new_unsigned(msg);
+    svm.send_transaction(txn).unwrap();
+}
+
+fn mint_to(svm: &mut LiteSVM, authority: &Pubkey, mint: &Pubkey, dest: &Pubkey, amount: u64) {
+    // MintTo: disc=7, amount
+    let mut data = vec![7u8];
+    data.extend_from_slice(&amount.to_le_bytes());
+
+    let ixn = Instruction {
+        program_id: TOKEN_PROGRAM,
+        accounts: vec![
+            AccountMeta::new(*mint, false),
+            AccountMeta::new(*dest, false),
+            AccountMeta::new_readonly(*authority, true),
+        ],
+        data,
+    };
+
+    let msg = Message::new(&[ixn], Some(authority));
+    let txn = Transaction::new_unsigned(msg);
+    svm.send_transaction(txn).unwrap();
+}
+
+fn spl_token_transfer_instruction(
+    from: &Pubkey,
+    to: &Pubkey,
+    authority: &Pubkey,
+    amount: u64,
+) -> Instruction {
+    // Transfer: disc=3, amount
+    let mut data = vec![3u8];
+    data.extend_from_slice(&amount.to_le_bytes());
+
+    Instruction {
+        program_id: TOKEN_PROGRAM,
+        accounts: vec![
+            AccountMeta::new(*from, false),
+            AccountMeta::new(*to, false),
+            AccountMeta::new_readonly(*authority, true),
+        ],
+        data,
+    }
+}
+
+fn get_token_balance(svm: &LiteSVM, account: &Pubkey) -> u64 {
+    let acc = svm.get_account(account).unwrap();
+    // amount is at offset 64 in TokenAccount
+    unsafe { acc.data.as_ptr().add(64).cast::<u64>().read_unaligned() }
+}
